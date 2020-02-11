6014f7b064dce540732997f1c6c134292686a17d
E. Semenyachenko <ybsemeny@mts.ru>
2020-02-11 18:07:21 +0300

    VLAN display works

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
 import json
+import pika
 
 app = Flask(__name__)
+
+def _start_rabbit():
+    conn = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = conn.channel()
+    args = {'x-message-ttl': 5000}
+    channel.queue_declare(queue='devstate', arguments=args)
+    return channel
+
+rabbit_channel = _start_rabbit()
 
 
 @app.route('/')
@@ -11,8 +21,9 @@ def hello_world():
 
 @app.route('/get_info/switch')
 def switch_state():
-    state = {0: 1, 1: 1, 2: 2, 3: 2, 4: 2, 5: 1}
-    return json.dumps(state)
+    result = rabbit_channel.basic_get(queue="devstate", auto_ack=True)[2] or ''
+    return result
+
 
 
 if __name__ == '__main__':
--- static/main.js
+++ static/main.js
@@ -1,18 +1,40 @@
-var current_switch_state = {};
+spinner = '<div class="mdl-spinner mdl-js-spinner is-active is-upgraded" data-upgraded=",MaterialSpinner"><div class="mdl-spinner__layer mdl-spinner__layer-1"><div class="mdl-spinner__circle-clipper mdl-spinner__left"><div class="mdl-spinner__circle"></div></div><div class="mdl-spinner__gap-patch"><div class="mdl-spinner__circle"></div></div><div class="mdl-spinner__circle-clipper mdl-spinner__right"><div class="mdl-spinner__circle"></div></div></div><div class="mdl-spinner__layer mdl-spinner__layer-2"><div class="mdl-spinner__circle-clipper mdl-spinner__left"><div class="mdl-spinner__circle"></div></div><div class="mdl-spinner__gap-patch"><div class="mdl-spinner__circle"></div></div><div class="mdl-spinner__circle-clipper mdl-spinner__right"><div class="mdl-spinner__circle"></div></div></div><div class="mdl-spinner__layer mdl-spinner__layer-3"><div class="mdl-spinner__circle-clipper mdl-spinner__left"><div class="mdl-spinner__circle"></div></div><div class="mdl-spinner__gap-patch"><div class="mdl-spinner__circle"></div></div><div class="mdl-spinner__circle-clipper mdl-spinner__right"><div class="mdl-spinner__circle"></div></div></div><div class="mdl-spinner__layer mdl-spinner__layer-4"><div class="mdl-spinner__circle-clipper mdl-spinner__left"><div class="mdl-spinner__circle"></div></div><div class="mdl-spinner__gap-patch"><div class="mdl-spinner__circle"></div></div><div class="mdl-spinner__circle-clipper mdl-spinner__right"><div class="mdl-spinner__circle"></div></div></div></div>';
 
 function get_current_switch_state() {
     $.get("/get_info/switch", function (data) {
     })
         .done(function (data) {
-            current_switch_state = data;
-            alert(data);
+            if (data !== '') {
+                $('#switch_state').html(create_port_table(JSON.parse(data)));
+            } else {
+                $('#switch_state').html(spinner);
+            }
         }).fail(function () {
-        current_switch_state = {};
-        alert("fail")
+            $('#switch_state').html(spinner);
+            console.log("fail")
     });
 }
 
 setInterval(() => get_current_switch_state(), 5000);
 
-
-//<div class="mdl-spinner mdl-js-spinner is-active"></div>
+function create_port_table(data) {
+    rows = '';
+    for (var port in data) {
+        rows += `<tr>\n` +
+            `      <th class='mdl-data-table__cell--non-numeric'>${port}</th>\n` +
+            `      <th>${data[port]}</th>\n` +
+            `    </tr>\n`;
+    }
+    table = `<table class='mdl-data-table mdl-js-data-table mdl-shadow--2dp'>\n` +
+        `  <thead>\n` +
+        `    <tr>\n` +
+        `      <th class='mdl-data-table__cell--non-numeric'>Port</th>\n` +
+        `      <th>VLAN</th>\n` +
+        `    </tr>\n` +
+        `  </thead>\n` +
+        `  <tbody>\n` +
+        `${rows}` +
+        `  </tbody>\n` +
+        `</table>`;
+    return table;
+}
